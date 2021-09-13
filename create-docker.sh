359893bc87063e5c31ec2d502e7862374b3bf2d6
XIZORG <leo.jek7@gmail.com>
2021-09-13 13:38:37 +0300

    add create docker script

---
+++ create-docker.sh
@@ -0,0 +1,5 @@
+mvn clean package
+sudo docker build -t user-mysql .
+sudo docker pull mysql:5.6
+sudo docker run --name mysql-standalone -e MYSQL_ROOT_PASSWORD=Ktjyfhlj2002 -e MYSQL_DATABASE=test -e MYSQL_USER=root -e MYSQL_PASSWORD=Ktjyfhlj2002 -d mysql:5.6
+sudo docker run -d -p 8089:8089 --name user-mysql --link mysql-standalone:mysql user-mysql
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 mvn clean package
 sudo docker build -t user-mysql .
 sudo docker rm user-mysql
-sudo docker container start 563421cd564e
+sudo docker container start mysql-standalone
 sudo docker run -d -p 8089:8089 --name user-mysql --link mysql-standalone:mysql user-mysql
 ./stop-docker.sh 
