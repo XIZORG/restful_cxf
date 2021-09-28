be4172b5106de1273808079844be89f247016800
XIZORG <leo.jek7@gmail.com>
2021-09-28 18:12:36 +0300

    changed validation

--- src/main/java/com/westbrain/sandbox/jaxrs/model/article/ArticleSubscribeRequest.java
+++ src/main/java/com/westbrain/sandbox/jaxrs/model/article/ArticleSubscribeRequest.java
@@ -2,10 +2,7 @@ package com.westbrain.sandbox.jaxrs.model.article;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 public class ArticleSubscribeRequest {
-    @NotBlank(message = "id cannot be blank!")
     private Long id;
 }
